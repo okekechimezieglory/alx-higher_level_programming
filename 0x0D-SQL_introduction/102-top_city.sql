27a7e33239ce17a36d2750fc12010590936cf2b5
okekechimezieglory <okekechimezieglory@gmail.com>
2024-05-21 13:05:08 -0700

    top city

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
-SELECT city, MAX(temperature) AS max_temperature
-FROM city_temperatures
+SELECT city, MAX(avg_emp) AS max_temperature
+FROM temperatures
 WHERE month IN ('July', 'August')
 GROUP BY city
 ORDER BY max_temperature DESC
